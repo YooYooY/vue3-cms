b1e8660c5312c8a5d85429f0842d7499b69a068c
chenwl <13751840799@163.com>
2024-01-23 18:14:00 +0800

    ✨ feat: add route

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="app">Hello world</div>
+  <div class="app">
+    <el-config-provider :locale="zhCn">
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+</script>
 
 <style scoped>
 .app {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
+import './assets/css/index.less'
 import 'normalize.css'
 
+import App from './App.vue'
+import router from '@/router'
+
+import registerIcons from '@/global/register-icons'
+
 const app = createApp(App)
+
+app.use(registerIcons)
+app.use(router)
 
 app.mount('#app')
